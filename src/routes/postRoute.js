dea41a967250979a10ea59b7c58da05eadc75dc0
Rodrigo Takashi Condo <rodrigo0takashi@gmail.com>
2022-08-26 19:05:31 -0300

    feat: criação endpoint GET /post/:id (requisito 14)

--- src/routes/postRoute.js
+++ src/routes/postRoute.js
@@ -10,4 +10,6 @@ postRoute.post('/', isAuth.validToken, isValid.validPost, postController.create)
 
 postRoute.get('/', isAuth.validToken, postController.getAll);
 
+postRoute.get('/:id', isAuth.validToken, postController.getById);
+
 module.exports = postRoute;
